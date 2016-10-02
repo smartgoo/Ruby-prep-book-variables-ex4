157a4a960e4b74b0f36298fca889bb6f05379741
Chuck Vallone <cvallone28@gmail.com>
2016-10-02 17:48:57 -0400

    ruby prep book variables ex4

---
+++ name.rb
@@ -0,0 +1,7 @@
+puts 'Hello, what is your first name?'
+first_name = gets.chomp + " "
+
+puts 'Okay, now what is your last name?'
+last_name = gets.chomp
+
+puts "Hello, " + first_name + last_name + "!"
